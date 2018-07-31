b107e225a7d2556c83a2f6a1f792140399ee4d91
jaeyong1.park <jaeyong1@naver.com>
2018-07-31 23:48:44 +0900

    ¬add java build script

---
+++ RPi_FieldController/build.sh
@@ -0,0 +1,33 @@
+echo "-----------------------------"
+echo "  JAVA Complier Version (1.8.xxxx)"
+echo "-----------------------------"
+echo $(javac -version)
+echo $(rm bin/ -rf)
+echo $(mkdir bin)
+echo $(rm release/ -rf)
+echo $(mkdir release)
+echo ""
+echo "-----------------------------"
+echo "  JAVA BUILD (.java)"
+echo `pwd`
+echo "-----------------------------"
+echo "=== java source files ==="
+echo $(find -name "*.java")
+echo $(find -name "*.java" > "sources.txt")
+echo $(javac -d ./bin @sources.txt -cp ./lib/org.eclipse.paho.client.mqttv3_1.1.0.jar:.)
+echo "=== complied classes ==="
+echo $(find ./bin -name "*.class")
+echo ""
+echo "-----------------------------"
+echo "  Packaging JAR (release/)"
+echo "-----------------------------"
+echo $(cp lib/org.eclipse.paho.client.mqttv3_1.1.0.jar release/)
+echo $(jar cvfm release/RPi_FieldController.jar Manifest.txt -C bin/ .)
+echo ""
+echo ""
+echo "-----------------------------"
+echo "  RUN (release/)"
+echo "-----------------------------"
+echo "To run.."
+echo "java -cp org.eclipse.paho.client.mqttv3_1.1.0.jar:RPi_FieldController.jar RPi_FieldController"
+echo ""
